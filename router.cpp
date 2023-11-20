e0a97c9acaa79bd0801a5b09031fdd2088f109b4
folkert van heusden <mail@vanheusden.com>
2023-11-20 14:49:06 +0100

    output tweak

--- router.cpp
+++ router.cpp
@@ -30,6 +30,7 @@ void router(const QueueHandle_t q, Print *const p, std::vector<target *> *const 
 
 			is_valid = a25->get_valid();
 			p->printf("%s -> %s (%d for %d - %s)", a25->get_from().to_str().c_str(), a25->get_to().to_str().c_str(), is_valid, unwrapped.has_value(), a25->get_invalid_reason().c_str());
+			p->println(F(""));
 			delete a25;
 		}
 
@@ -44,6 +45,7 @@ void router(const QueueHandle_t q, Print *const p, std::vector<target *> *const 
 				dummy += myformat("%02x ", v);
 
 			p->printf("Invalid packet: %s", dummy.c_str());
+			p->println(F(""));
 		}
 
 		delete msg.data;
