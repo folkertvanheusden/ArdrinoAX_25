591de10bd22e3d0242401a9939b7f1f40ab14206
folkert van heusden <mail@vanheusden.com>
2023-11-20 14:47:37 +0100

    aprs oe_ messages are not invalid

--- router.cpp
+++ router.cpp
@@ -10,15 +10,28 @@ void router(const QueueHandle_t q, Print *const p, std::vector<target *> *const 
 	if (xQueueReceive(q, &msg, 200 / portTICK_PERIOD_MS) == pdTRUE) {
 		ax25_packet *a25 = nullptr;
 
-		auto unwrapped = unwrap_kiss(*msg.data);
-		if (unwrapped.has_value())
-			a25 = new ax25_packet(unwrapped.value());
-		else
-			a25 = new ax25_packet(*msg.data);
+		bool is_valid = false;
 
-		bool is_valid = a25->get_valid();
-		p->printf("%s -> %s (%d for %d - %s)", a25->get_from().to_str().c_str(), a25->get_to().to_str().c_str(), is_valid, unwrapped.has_value(), a25->get_invalid_reason().c_str());
-		delete a25;
+		if (msg.data->at(0) == 0x3c) {  // APRS OE_ msg
+			is_valid = true;
+
+			std::string debug;
+			for(size_t i=3; i<msg.data->size(); i++)
+				debug += char(msg.data->at(i));
+
+			p->println(debug.c_str());
+		}
+		else {
+			auto unwrapped = unwrap_kiss(*msg.data);
+			if (unwrapped.has_value())
+				a25 = new ax25_packet(unwrapped.value());
+			else
+				a25 = new ax25_packet(*msg.data);
+
+			is_valid = a25->get_valid();
+			p->printf("%s -> %s (%d for %d - %s)", a25->get_from().to_str().c_str(), a25->get_to().to_str().c_str(), is_valid, unwrapped.has_value(), a25->get_invalid_reason().c_str());
+			delete a25;
+		}
 
 		if (is_valid) {
 			for(auto & t: *targets)
